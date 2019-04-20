49162973df5979e294fecae55d464ba9dd8f8dd5
Aaron Geller <ageller@williamson.local>
2019-04-20 16:11:38 -0500

    testing vlc

--- src/js/externalAjax.js
+++ src/js/externalAjax.js
@@ -9,21 +9,17 @@ function flyWWT(url){
 		}
 	}
 
-	// var popup = window.open(url,"WWT", "width=200,height=100");
-	// setTimeout(function() {popup.close();}, 1000); //I want to make this fire onload, but it won't let me
-	// //popup.blur(); //doesn't work
-	// //window.focus();
 }
 function launchVLC3D(movie){
 	//testing for now
+	movie="/Users/svladler/AstroConversationMedia/Movies3D_TopBottom/Stars/Sun/EUVI_171_Angstroms-TB2.mov"
 
-	//try with Ajax
 
 	//clear the playlist
 	var url1 = "http://SVL3DTV.adlerplanetarium.org:8080/requests/status.xml?command=pl_empty";
 	
 	//add a movie to playlist
-	var url2 = "http://SVL3DTV.adlerplanetarium.org:8080/requests/status.xml?command=in_enqueue&input=/Users/svladler/AstroConversationMedia/Movies3D_TopBottom/Stars/Sun/EUVI_171_Angstroms-TB2.mov";
+	var url2 = "http://SVL3DTV.adlerplanetarium.org:8080/requests/status.xml?command=in_enqueue&input="+movie;
 	
 	//play the playlist
 	var url3 = "http://SVL3DTV.adlerplanetarium.org:8080/requests/status.xml?command=pl_play";
@@ -31,24 +27,28 @@ function launchVLC3D(movie){
 	//go to fullscreen
 	var url4 = "http://SVL3DTV.adlerplanetarium.org:8080/requests/status.xml?command=fullscreen"
 	
-	var http1 = new XMLHttpRequest();
-	http1.open("GET", url1);
-	http1.send();
+	function runVLC(url){
+		var http = new XMLHttpRequest();
+		http.open("GET", url);
+		http.send();
+		return http;
+	}
+
+
+	var http1 = runVLC(url1);
+
 	http1.onreadystatechange = function(){
 		if (this.readyState == 4 && this.status == 200){
-			var http2 = new XMLHttpRequest();
-			http2.open("GET", url2);
-			http2.send();
+			var http2 = runVLC(url2);
+
 			http2.onreadystatechange = function(){
 				if (this.readyState == 4 && this.status == 200){
-					var http3 = new XMLHttpRequest();
-					http3.open("GET", url3);
-					http3.send();
+					var http3 = runVLC(url3);
+
 					http3.onreadystatechange = function(){
 						if (this.readyState == 4 && this.status == 200){
-							var http4 = new XMLHttpRequest();
-							http4.open("GET", url4);
-							http4.send();
+							var http4 = runVLC(url4);
+
 							http4.onreadystatechange = function(){
 								if (this.readyState == 4 && this.status == 200){
 									console.log('playing VLC 3D movie')
